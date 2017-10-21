42d0a2de958b63410d5b63f04c86f1af0d37d46c
josemanuelreviriego <josemanuel.reviriego.ciclos@gmail.com>
2017-10-21 20:50:31 +0200

    Ordenacion de Strings en C#

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Palabras que se van a ordenar
+            String[] lista =
+            {
+                    "pepe", "manuela", "abelardo", "ramon"
+                };
+
+            // Las ordenamos a partir del método Sort de la clase Array
+            Array palabras = lista;
+            Array.Sort(lista);
+
+            // Visualizamos las palabras ordenadas
+            foreach (string pal in lista)
+                Console.WriteLine(pal);
+
+            for(int i=0; i < 4; i++)
+            {
+                String palabra = lista[i];
+                Array.Sort(lista);
+                Console.WriteLine(lista[i]);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
