5bf64d83ad960169aac0e20db3bef682b87f3b1f
josemanuelreviriego <josemanuel.reviriego.ciclos@gmail.com>
2017-11-21 19:49:08 +0100

    Ejercicio de camaras con clases

---
+++ Camaras/Camaras/CamaraIP.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camaras
+{
+    class CamaraIP
+    {
+        private string marca, objetivo, direccionMAC, direccionIP;
+        private int peso;
+        
+        public CamaraIP()
+        {
+            marca = "";
+            objetivo = "";
+            direccionMAC = "";
+            direccionIP = "";
+            peso = 0;
+        }
+        
+        public CamaraIP(string marca, string objetivo, string direccionMAC, string direccionIP, int peso)
+        {
+            this.marca = marca;
+            this.objetivo = objetivo;
+            this.direccionMAC = direccionMAC;
+            this.direccionIP = direccionIP;
+            this.peso = peso;
+        }
+
+        public string DireccionIP
+        {
+            get
+            {
+                return direccionIP;
+            }
+
+            set
+            {
+                direccionIP = value;
+            }
+        }
+
+        public string DireccionMAC
+        {
+            get
+            {
+                return direccionMAC;
+            }
+
+            set
+            {
+                direccionMAC = value;
+            }
+        }
+
+        public int Peso
+        {
+            get
+            {
+                return peso;
+            }
+
+            set
+            {
+                peso = value;
+            }
+        }
+
+        public string Escribir()
+        {
+            return "Camara de marca: " + marca +
+                " con el objetivo " + objetivo +
+                " con esta direccionIP " + direccionIP +
+                " con esta direccionMAC " + direccionMAC +
+                " y este peso " + peso;
+        }
+
+        public void vaciar()
+        {
+            marca = "";
+            objetivo = "";
+            direccionIP = "";
+            direccionMAC = "";
+            peso = 0;
+        }
+    }
+}
---
+++ Camaras/Camaras/CamaraWEB.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camaras
+{
+    class CamaraWEB
+    {
+        private string marca, objetivo, resolucion;
+        private int peso, zoom;
+
+        public CamaraWEB()
+        {
+            marca = "";
+            objetivo = "";
+            resolucion = "";
+            peso = 0;
+            zoom = 0;
+        }
+
+        public CamaraWEB(string marca, string objetivo, string resolucion, int peso, int zoom)
+        {
+            this.marca = marca;
+            this.objetivo = objetivo;
+            this.resolucion = resolucion;
+            this.peso = peso;
+            this.zoom = zoom;
+        }
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+
+            set
+            {
+                marca = value;
+            }
+        }
+
+        public string Resolucion
+        {
+            get
+            {
+                return resolucion;
+            }
+
+            set
+            {
+                resolucion = value;
+            }
+        }
+
+        public string Escribir()
+        {
+            return "Camara de marca: " + marca +
+                " con el objetivo " + objetivo +
+                " con esta resolucion " + resolucion +
+                " con este peso " + peso +
+                " y este zoom " + zoom;
+        }
+
+        public void vaciar()
+        {
+            marca = "";
+            objetivo = "";
+            resolucion = "";
+            peso = 0;
+            zoom = 0;
+        }
+    }
+}
