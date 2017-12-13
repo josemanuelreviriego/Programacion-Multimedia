21ca19c67a162ac0aff9083a948abc3585e47be5
josemanuelreviriego <josemanuel.reviriego.ciclos@gmail.com>
2017-12-13 23:18:39 +0100

    Ensayos con List

---
+++ PruebasConListas/PruebasConListas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasConListas
+{
+    class Program
+    {
+        public static void imprimir(string s)
+        {
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+
+            List<string> nombre = new List<string>();
+            nombre.Add("Maria");
+            nombre.Add("Pepe");
+            nombre.Remove("Maria");
+            nombre.Add("Jose");
+            nombre.Add("Ramon");
+
+            nombre.ForEach(delegate (string name)
+            {
+                Console.WriteLine("Estos son mis nombres: " + name);
+                Console.ReadKey();
+            });
+
+            nombre.ForEach(imprimir);
+            
+                
+            
+            
+
+            
+        }
+    }
+}
